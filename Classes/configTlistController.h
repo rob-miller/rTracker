a2c4a7d618ba9215029ee0453a9816af3a2241cc
rob miller <rob@realidata.com>
2011-01-25 21:43:28 +0000

    cleaned up directory structure
    git-svn-id: file:///Users/rob/proj-svn/rTracker/trunk@34 baea2ad3-8b63-4a7b-9cb0-ce70295316be

---
+++ Classes/configTlistController.h
@@ -0,0 +1,33 @@
+//
+//  configTlistController.h
+//  rTracker
+//
+//  from this screen the user can move, delete, copy, or select for edit (modify) one of the existing trackers
+//
+//  Created by Robert Miller on 06/05/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "trackerList.h"
+
+#define SegmentEdit 0
+#define SegmentCopy 1
+#define SegmentMoveDelete 2
+
+
+@interface configTlistController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate>
+//@interface configTlistController : UITableViewController
+{
+
+	trackerList *tlist;
+	UITableView *table;
+}
+
+@property (nonatomic, retain) trackerList *tlist;
+@property (nonatomic, retain) IBOutlet UITableView *table;
+
+- (IBAction) btnExport;
+- (IBAction) modeChoice:(id)sender;
+
+@end
